217a3ce32384f684466d9199e2e30bd1661112d5
doyoon-c-dev <doyoon.dev@gmail.com>
2026-09-25 19:28:08 +0900

    Update WebConfig.java

--- backend/src/main/java/com/trpg_printalbe_pdf_maker/trpg_printable_pdf_maker/config/WebConfig.java
+++ backend/src/main/java/com/trpg_printalbe_pdf_maker/trpg_printable_pdf_maker/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-            .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+            .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "https://trpg-printable-pdf-maker.vercel.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
                 .allowCredentials(true);
